72bbc2d809eebda14b315875ca06b0ac3165f308
Pere Antoni Bennàssar Albons <pereantonibennassar@gmail.com>
2020-07-28 18:57:40 +0200

    xat prova1

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 var contador=1;
 
@@ -9,6 +11,23 @@ app.get('/prova/',function(req,res){
         res.send('Hi world!');
         console.log('somebody ' + contador);
         contador++;
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 app.listen('3000', function() {
